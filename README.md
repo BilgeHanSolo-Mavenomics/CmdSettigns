e84be5891ffa81001f79ac9393f72ab0a2526590
BilgeHanSolo <bil@mavenomics.com>
2017-10-11 14:53:44 -0400

    Added new shourcuts, cleaned up code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CmdSettigns
 
 run with cmd.exe /K env.cmd
+
+## Pin taskbar
+create cmd shortcut
+Target C:\Windows\System32\cmd.exe /K env.cmd
+Start in C:\Repo Directory\CmdSettigns
--- env.cmd
+++ env.cmd
@@ -1,3 +1,16 @@
+@echo off
 title "BilgeHanSolo@MilleniumFalcon"
+
 doskey np=notepad++.exe $*
+doskey subl="C:\Program Files\Sublime Text 2\sublime_text.exe" $*
+
 doskey gitbash=start "" "%SYSTEMDRIVE%\Program Files (x86)\Git\bin\sh.exe" --login
+
+doskey desktop=cd %HOMEPATH%\Desktop
+doskey repos=cd %HOMEPATH%\Desktop\Repos
+doskey webserver=python -m SimpleHTTPServer 8000
+doskey workspace=cd %HOMEPATH%\Desktop\Repos\MiscDemos\WebCompositing\WebCompositing
+
+doskey cat=type $*
+doskey clear=cls
+doskey ls=dir /A /B $*
